c59c38cf0182db85fff996458366f642f6946033
TonnyHawk <anton.veremko@gmail.com>
2020-11-29 07:52:05 +0200

    slider changes
    *swiper deleted from the project
    *owl carousel does the all job by itself
    *html structure of the .comment-slider was changed a lil bit
    *styles of .comment-slider was also improved

--- src/sass/sections/brands-slider/brands-slider.js
+++ src/sass/sections/brands-slider/brands-slider.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
-   $(".owl-carousel").owlCarousel({
-    items: 6,
-    loop: true,
-    responsive:{
-      0:{
-          items:2.5
-      },
-      500:{
-        items: 3.5
-      },
-      800:{
-        items: 5
-      },
-      850:{
-        items: 6
+    $(".brands-slider").owlCarousel({
+      items: 6,
+      loop: true,
+      responsive:{
+        0:{
+            items:2.5
+        },
+        500:{
+          items: 3.5
+        },
+        800:{
+          items: 5
+        },
+        850:{
+          items: 6
+        }
       }
-    }
-   });
+    });
  });
--- src/sass/sections/comment-slider/comment-slider.js
+++ src/sass/sections/comment-slider/comment-slider.js
@@ -1,8 +1,18 @@
-new Swiper('.comment-slider', {
-   // Navigation arrows
-   navigation: {
-      nextEl: '.comment-slider__btn-next',
-      prevEl: '.comment-slider__btn-prev',
-   },
-   loop: true
+$(document).ready(function(){
+
+   var anonymSlider = $("#anonym-slider .comment-slider");
+   anonymSlider.owlCarousel({
+      loop: true,
+      items: 1,
+      dots: false,
+   });
+
+   // Custom Navigation Events
+   $("#anonym-slider .comment-slider__btn-next").click(function(){
+      anonymSlider.trigger('next.owl.carousel');
+   })
+   $("#anonym-slider .comment-slider__btn-prev").click(function(){
+      anonymSlider.trigger('prev.owl.carousel');
+   })
+
 });
